3c262c1c98322b67f000c2810fea7eb46007a2d4
Sean Pianka <pianka@eml.cc>
2017-05-17 03:57:48 -0400

    removed old main code, adding lib dir

--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,5 @@ int main()
     MiniaGame game(GameWindow::w640h480);
     game.start();
 
-    /*
-    std::vector<sf::CircleShape> batch_circles;
-
-    sf::CircleShape head(100.f);
-    head.setFillColor(sf::Color::Green);
-
-    batch_circles.push_back(head);
-
-    while (window.isOpen())
-    {
-        process_events(window, batch_circles);
-    }
-    */
-
     return 0;
 }
-
-/*
-void process_events(GameWindow& window, std::vector<sf::CircleShape>& batch_circles)
-{
-    sf::Event event;
-
-    while (window.pollEvent(event))
-    {
-        if (event.type == sf::Event::Closed)
-        {
-            window.close();
-        }
-    }
-
-    window.clear();
-    for (sf::CircleShape& shape : batch_circles)
-    {
-        window.draw(shape);
-    }
-    window.display();
-}
-*/
